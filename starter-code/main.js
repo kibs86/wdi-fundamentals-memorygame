26a8ca3d33a859f6f14aa9553b60504e2649cae6
Kara Langlois <kara.langlois@libertymutual.com>
2016-09-12 12:49:06 -0400

    Added conditionals

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,14 @@
 var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
