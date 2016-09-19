d8b9d49e7ba58525762a1f93cba0c3dac62050e6
Kara Langlois <kara.langlois@libertymutual.com>
2016-09-19 17:02:51 -0400

    Changes for unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,21 @@
+var gameBoard = document.getElementById('game-board');
+var maxCards = 4;
+var createBoard = function() {
+	for (var i = 1; i <=maxCards; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	};
+}
+
+createBoard();
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else if (cardOne === cardTwo) {
 	alert("You found a match!");
@@ -11,4 +23,7 @@ if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-}
+} */
+
+/* TO DO
+allow users to choose how many cards they want to play with greater than 1 */
